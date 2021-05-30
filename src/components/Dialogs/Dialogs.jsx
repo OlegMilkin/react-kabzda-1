aea5a3987daee4a4e2daf0449c4d5884c8b5d49b
OlegMilkin <olegek.milkin@gmail.com>
2021-05-30 19:51:24 +0300

    Lesson 22 routing exact

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,30 @@
 import React from "react";
 import classes from "./Dialogs.module.css";
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
   return (
-    <p>Dialogs</p>
+    <div className={classes.dialogs}>
+      <div className={classes.dialogsItems}>
+        <div className={classes.item + ' ' + classes.active}>
+          <NavLink to="/dialogs/1">Dimych</NavLink>
+        </div>
+        <div className={classes.item}>
+          <NavLink to="/dialogs/2">Oleg</NavLink>
+        </div>
+        <div className={classes.item}>
+          <NavLink to="/dialogs/3">Sasha</NavLink>
+        </div>
+        <div className={classes.item}>
+          <NavLink to="/dialogs/4">Ruben</NavLink>
+        </div>
+      </div>
+      <div className={classes.messages}>
+        <div className={classes.message}>Hi</div>
+        <div className={classes.message}>How is your IT?</div>
+        <div className={classes.message}>YO</div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import classes from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
-    return <nav className={classes.nav}>
+    return (
+      <nav className={classes.nav}>
         <div>
-            <a href='/profile'>Profile</a>
+            <NavLink to='/profile' activeClassName={classes.activeLink}>Profile</NavLink>
         </div>
         <div>
-            <a href='/dialogs'>Messages</a>
-        </div><div>
-            <a href='/news'>News</a>
+            <NavLink to='/dialogs' activeClassName={classes.activeLink}>Messages</NavLink>
         </div>
         <div>
-            <a href='/music'>Music</a>
+            <NavLink to='/news' activeClassName={classes.activeLink}>News</NavLink>
         </div>
         <div>
-            <a href='/settings'>Settings</a>
+            <NavLink to='/music' activeClassName={classes.activeLink}>Music</NavLink>
         </div>
-    </nav>
+        <div>
+            <NavLink to='/settings' activeClassName={classes.activeLink}>Settings</NavLink>
+        </div>
+      </nav>
+    )
 }
 
 export default Navbar;
